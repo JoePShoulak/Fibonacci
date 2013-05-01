b2fc4855508dd7e10651524de77e18705b4d2b68
joepshoulak <your_email@example.com>
2013-05-01 08:12:05 -0500

    comment my code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
-#include <iostream>
-#include <vector>
-#include <math.h>
+#include <iostream> // For IO
+#include <math.h> // For exponents
 
+// Define the folden ratio in the preprocessor
 #define PHI ( ( 1 + pow(5, 0.5) ) / 2)
 
-using std::vector;
 using std::cout;
 
 int fibonacci(int it) {
+  /* Define the Nth Fibonacci number as the result of the binet formula
+     with the iterator as the argument */
   float  fn = ( pow(PHI, it) - pow(-PHI, -it) ) / pow(5, 0.5);
+  // Output the term count and the term's value
   cout << it << ": " << fn << "\n";
   return 0;
 }
 
 int main() {
+  // Start at the first number
   int it = 1;
   while (it <= 15) {
+    // Call the Fibonacci function
     fibonacci(it);
+    // Progress the iterator.
     ++it;  
   }
   return 0;
