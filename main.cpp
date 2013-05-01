76d417eb978fa62e44e7220d8421ea5382fe6337
joepshoulak <your_email@example.com>
2013-05-01 08:39:42 -0500

    add input for number of fibonacci numbers to be viewed

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #define PHI ( ( 1 + pow(5, 0.5) ) / 2)
 
 using std::cout;
+using std::cin;
 
 int fibonacci(int it) {
   /* Define the Nth Fibonacci number as the result of the binet formula
@@ -18,7 +19,10 @@ int fibonacci(int it) {
 int main() {
   // Start at the first number
   int it = 1;
-  while (it <= 15) {
+  int limit;
+  cout << "How many numbers would you like to view? ";
+  cin >> limit;
+  while (it <= limit) {
     // Call the Fibonacci function
     fibonacci(it);
     // Progress the iterator.
